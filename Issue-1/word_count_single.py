4adecc70867554cdab1470fa05ed9a28eca6bb49
Rithvik Reddy <rithvkalkanti@gmail.com>
2025-08-31 22:32:36 +0530

    Add word count script to process large text files

---
+++ Issue-1/word_count_single.py
@@ -0,0 +1,15 @@
+import time
+files = ["500MBfile.txt", "1GBfile.txt", "2GBfile.txt"]
+
+for file in files:
+    ## Also calculate the time per file
+    start_time = time.time()
+    word_count = {}
+    with open(file, "r") as f:
+        for line in f:
+            words = line.split()
+            for word in words:
+                word_count[word] +=1
+
+    print(f"Word count for {file}: {len(word_count)} unique words")
+    print(f"Time taken to process {file}: {time.time() - start_time} seconds")
